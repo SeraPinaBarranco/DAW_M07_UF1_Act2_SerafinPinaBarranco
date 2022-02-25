3a0b1e8b8edf8efa28a8c108c230f4d3f11d5e4f
Serafín Piña <arespin23@gmail.com>
2022-02-25 22:48:31 +0100

    Fin de la actividad, falta probar y comentar

--- nota/notas.php
+++ nota/notas.php
@@ -1,23 +1,24 @@
 <?php
 
 $nota = $_POST["nota"];
-
-if(empty($_POST["nota"])){
-    $nota="cero";
-}
-
 if(empty($nota)){
-    echo("Tienes que poner un nota!!");
+  echo("Introduce un valor");
+}elseif($nota === "0"){   //si el valor es cero al se puede considerar como empty
+  echo("Suspenso");
+}elseif(filter_var($nota,FILTER_VALIDATE_FLOAT)){
+  $f_nota = floatval($nota);
+  
+  if($f_nota >= 0.1 and $nota < 5)echo("Suspenso");
+  if($f_nota >= 5 and $nota < 7)echo("Aprobado");
+  if($f_nota >= 7 and $nota < 9)echo("Notable");
+  if($f_nota >= 9 and $nota < 10)echo("Excelente");
+  if($f_nota == 10)echo("Matrícula de honor");
+  if($f_nota > 10)echo("Introduce una nota válida");
+
+}else{
+  echo("La nota introducida no es correcta");
 }
-else{
-   //$nota= intval($_POST["nota"]);
-   if($nota === "cero")echo("Suspenso");
-   if($nota >= 0.1 and $nota < 5)echo("Suspenso");
-   if($nota >= 5 and $nota < 7)echo("Aprobado");
-   if($nota >= 7 and $nota < 9)echo("Notable");
-   if($nota >= 9 and $nota < 10)echo("Excelente");
-   if($nota == 10)echo("Matrícula de honor");
-}
-  echo '<p><a href="' .$_SERVER['HTTP_REFERER']. '">Atras</a></p>';
+
+header("Refresh: 2; url=" . $_SERVER["HTTP_REFERER"]);
 
 ?>
