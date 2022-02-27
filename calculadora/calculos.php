c2a80c5e5edfb35284782c8a1bc312f8cf118528
Serafín Piña <arespin23@gmail.com>
2022-02-28 00:11:25 +0100

    division

--- calculadora/calculos.php
+++ calculadora/calculos.php
@@ -1,27 +1,81 @@
 <?php
 
-    if(empty($_POST["num1"]) || empty($_POST["num2"]))exit("Ningun numero puede venir vacio!!");
-
-    $num1=$_POST["num1"];
-    $num2=$_POST["num2"];
+    // $num1=$_POST["num1"];
+    // $num2=$_POST["num2"];  
     
-    $n1 = intval($num1);
-    $n2 = intval($num2);
+    // if($num1 !== "0" || $num2 !== "0"){
+    //     if(empty($_POST["num1"]) || empty($_POST["num2"])){
+    //         salir();
+    //         exit("Ningun numero puede venir vacio!!</br>");
+    //     }        
+    // }
+
+    // comprobar_numero($num1);
+    // comprobar_numero($num2);
     
+    // $n1 = intval($num1);
+    // $n2 = intval($num2);
+    //if(!empty($_POST["operacion"])){
+        // $operacion= $_POST["operacion"];
+
+    if($_POST["num1"] != "" && $_POST["num2"] != ""){
+        $num1=$_POST["num1"];
+        $num2=$_POST["num2"];
+
+        if(!empty($_POST["operacion"])){
+            $operacion= $_POST["operacion"];
+        }else{
+            salir();
+            exit("no ha seleccionado una operación!!</br>");
+        }
+
+        //comprobar_numero($num1);
+        //comprobar_numero($num2);
+
+        if($operacion == "suma"){
+            echo ("El resultado de la suma es ". ($num1 + $num2)) ;
+        }else if($operacion == "resta"){
+            echo ("El resultado de la resta es ". ($num1 - $num2)) ;
+        }else if($operacion == "multi"){
+            echo ("El resultado de la multiplicacion es ". ($num1 * $num2)) ;
+        }else if($operacion == "divi"){
+            if($num2= "0"){
+                echo ("El resultado de la division es ". sprintf("%.2f", ($num1 / $num2))) ;
+            }else{
+                salir();
+                exit("El denominador no puede ser 0");
+            }            
+        }
+    }else{
+        echo("Ingresa todos los valores!!</br>");
+        salir();
+    }
+        
+
+    //     salir();
+
+    // }else{
+    //     echo("no ha seleccionado una operación!!</br>");
+    //     salir();
+     //}
     
-    if(empty($_POST["operacion"]))exit("no ha seleccionado una operación!!");
-    $operacion= $_POST["operacion"];
+   
 
-    if($operacion == "suma")echo ("El resultado de la suma es ". ($n1 + $n2)) ;
-    if($operacion == "resta")echo ("El resultado de la resta es ". ($n1 - $n2)) ;
-    if($operacion == "multi")echo ("El resultado de la multiplicacion es ". ($n1 * $n2)) ;
-    if($operacion == "divi")echo ("El resultado de la division es ". sprintf("%.2f", ($n1 / $n2))) ;
-
-    //echo "<a href=”’.$_SERVER['HTTP_REFERER'].’”>Ejemplo de Botón Regresar en PHP</a>’;
-
-    echo '<p><a href="' .$_SERVER['HTTP_REFERER']. '">Atras</a></p>';
-
-
-
-
+    function comprobar_numero($n){      
+        if(!filter_var($n,FILTER_VALIDATE_FLOAT)){
+            salir();
+            exit("Alguno de los valores tienen formato incorrecto (el signo digital es el punto)!!</br>");
+        }
+    }
+    
+    // function comprobar_operacion($op){
+    //     if(empty($op)){
+            
+    //         salir();
+    //     }
+    // }
+    
+    function salir(){        
+        header("Refresh: 4; url=" . $_SERVER["HTTP_REFERER"]);
+    }
 ?>
--- nota/notas.php
+++ nota/notas.php
@@ -1,20 +1,17 @@
 <?php
 
 $nota = $_POST["nota"];
-if(empty($nota)){
-  echo("Introduce un valor");
-}elseif($nota === "0"){   //si el valor es cero al se puede considerar como empty
+
+if($nota === "0"){   //si el valor es cero al se puede considerar como empty
   echo("Suspenso");
 }elseif(filter_var($nota,FILTER_VALIDATE_FLOAT)){
   $f_nota = floatval($nota);
-  
   if($f_nota >= 0.1 and $nota < 5)echo("Suspenso");
   if($f_nota >= 5 and $nota < 7)echo("Aprobado");
   if($f_nota >= 7 and $nota < 9)echo("Notable");
   if($f_nota >= 9 and $nota < 10)echo("Excelente");
   if($f_nota == 10)echo("Matrícula de honor");
   if($f_nota > 10)echo("Introduce una nota válida");
-
 }else{
   echo("La nota introducida no es correcta");
 }
